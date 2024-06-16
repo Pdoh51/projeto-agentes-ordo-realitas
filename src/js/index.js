d139663d236ae2f80fad2d330191fd4ccc84d22e
PedroHss <161982260+Pdoh51@users.noreply.github.com>
2024-06-16 15:11:35 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+const agentes = document.querySelectorAll(".botao")
+
+const personagens = document.querySelectorAll('.personagem')
+
+agentes.forEach((botao, indice) => {
+botao.addEventListener("click", () => {
+
+    desselecionarBotao()
+    desselecionarPersonagem()
+botao.classList.add("selecionado")
+
+personagens[indice].classList.add('selecionado')
+
+})
+})
+
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector('.personagem.selecionado')
+
+    personagemSelecionado.classList.remove('selecionado')
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado")
+
+    botaoSelecionado.classList.remove('selecionado')
+}
